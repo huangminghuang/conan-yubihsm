3e360bb550977b210729c1509b03e0b116c26288
Huang-Ming Huang <huangh@objectcomputing.com>
2019-09-11 13:44:28 -0500

    add build_policy

--- build.py
+++ build.py
@@ -6,6 +6,6 @@ from cpt.packager import ConanMultiPackager
 import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(build_policy="missing")
     builder.add_common_builds(pure_c=True)
     builder.run()
